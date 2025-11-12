e80b7a68ac24cc6227d3fb258057e380ec886b29
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-11-13 05:48:49 +0600

    connect mongodb to server

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // middlewere
 app.use(cors());
 app.use(express.json());
+// CVqbHVCx84e5KqKy
+// GreenChain
+
+// mongodb uri
+const uri =
+  "mongodb+srv://GreenChain:CVqbHVCx84e5KqKy@cluster0.96upm.mongodb.net/?appName=Cluster0";
+// mongobd client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // running the server
 app.get("/", (req, res) => {
