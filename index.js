2170c9e82dc8b9fc2c44a1199e35b0ff73384ea0
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-11-13 06:41:38 +0600

    added post apis for add crops

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("greenDB");
+    const cropsCollection = database.collection("crops");
+
+    // crops apis
+    app.post("/addCrops", async (req, res) => {
+      const newCrops = req.body;
+      const result = await cropsCollection.insertOne(newCrops);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +42,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
